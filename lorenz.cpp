4a1a2e27b9b6bdca3389c951ce1d455310fc1bab
Daniel <daniel.lima@inmetrics.com.br>
2023-02-25 14:03:13 +0100

    generalize for multiple trajectories

--- lorenz.cpp
+++ lorenz.cpp
@@ -24,26 +24,45 @@ int main()
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0, 30.0);
 
-    // Initialize vectors
-    arma::vec x_t = arma::zeros(N);
-    arma::vec y_t = arma::zeros(N);
-    arma::vec z_t = arma::zeros(N);
+    // X_t, Y_t, Z_t will hold 100 time series
+    arma::mat X_t(N, 100);
+    arma::mat Y_t(N, 100);
+    arma::mat Z_t(N, 100);
 
-    // Initial conditions
-    double x_0 = dis(gen) - 15.0;
-    double y_0 = dis(gen) - 15.0;
-    double z_0 = dis(gen) - 15.0;
+    // vector to hold the 100 initial conditions
+    arma::vec X_0(100);
+    arma::vec Y_0(100);
+    arma::vec Z_0(100);
+
+    // Fill Initial conditions
+    for (int i = 0; i < 100; i++)
+    {
+        X_0(i) = dis(gen) - 15.0;
+        Y_0(i) = dis(gen) - 15.0;
+        Z_0(i) = dis(gen) - 15.0;
+    }
 
     // evolve the system
-    for (unsigned int i = 0; i < N; i++)
+    double X_0_ptc;
+    double Y_0_ptc;
+    double Z_0_ptc;
+    for (unsigned int step = 0; step < N; step++)
     {
-        x_t(i) = ode_solver(x_0, y_0, z_0, 0);
-        y_t(i) = ode_solver(x_0, y_0, z_0, 1);
-        z_t(i) = ode_solver(x_0, y_0, z_0, 2);
+        for (int ptc = 0; ptc < 100; ptc++) // loop over particles = different trajectories
+        {
+            // there is probably a better way to do the following
+            X_0_ptc = X_0(ptc);
+            Y_0_ptc = Y_0(ptc);
+            Z_0_ptc = Z_0(ptc);
 
-        x_0 = x_t(i);
-        y_0 = y_t(i);
-        z_0 = z_t(i);
+            X_t(step, ptc) = ode_solver(X_0_ptc, Y_0_ptc, Z_0_ptc, 0);
+            Y_t(step, ptc) = ode_solver(X_0_ptc, Y_0_ptc, Z_0_ptc, 1);
+            Z_t(step, ptc) = ode_solver(X_0_ptc, Y_0_ptc, Z_0_ptc, 2);
+
+            X_0(ptc) = X_t(step, ptc);
+            Y_0(ptc) = Y_t(step, ptc);
+            Z_0(ptc) = Z_t(step, ptc);
+        }
     }
 
     // write to file
@@ -56,15 +75,21 @@ int main()
     ofile << std::setw(width) << std::setprecision(prec) << "t";
     ofile << std::setw(width) << std::setprecision(prec) << "x";
     ofile << std::setw(width) << std::setprecision(prec) << "y";
-    ofile << std::setw(width) << std::setprecision(prec) << "z" << std::endl;
+    ofile << std::setw(width) << std::setprecision(prec) << "z";
+    ofile << std::setw(width) << std::setprecision(prec) << "particle"
+          << std::endl;
 
     // data
-    for (int i = 0; i < N; i++)
+    for (int ptc = 0; ptc < 100; ptc++)
     {
-        ofile << std::setw(width) << std::setprecision(prec) << T(i) << ",";
-        ofile << std::setw(width) << std::setprecision(prec) << x_t(i) << ",";
-        ofile << std::setw(width) << std::setprecision(prec) << y_t(i) << ",";
-        ofile << std::setw(width) << std::setprecision(prec) << z_t(i) << std::endl;
+        for (int step = 0; step < N; step++)
+        {
+            ofile << std::setw(width) << std::setprecision(prec) << T(step) << ",";
+            ofile << std::setw(width) << std::setprecision(prec) << X_t(step, ptc) << ",";
+            ofile << std::setw(width) << std::setprecision(prec) << Y_t(step, ptc) << ",";
+            ofile << std::setw(width) << std::setprecision(prec) << Z_t(step, ptc) << ",";
+            ofile << std::setw(width) << std::setprecision(prec) << ptc << std::endl;
+        }
     }
 
     return 0;
