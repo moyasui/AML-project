a55ef68fa925c1380d092419fae6dc96210fffa7
KlogeBukser <keranchen@protonmail.com>
2023-05-14 19:23:57 +0200

    added spiral_rnn.ipynb

--- src/visualisers/pg_visualiser.py
+++ src/visualisers/pg_visualiser.py
@@ -2,10 +2,13 @@ import pygame
 import pandas as pd
 import numpy as np
 
-def py_visualiser(filename=None, seq_pos=None, indx=8,):
+def py_visualiser(dataset=None, filename=None, seq_pos=None, indx=8,):
+    # TODO: 
 # Set up Pygame
     pygame.init()
     clock = pygame.time.Clock()
+
+    has_pred = True
     if seq_pos is None:
         has_pred = False
 
@@ -15,12 +18,17 @@ def py_visualiser(filename=None, seq_pos=None, indx=8,):
     pygame.display.set_caption('3D Function Animation')
 
     # Read CSV file 'src/csvs/lorenz.csv'
-    if filename is None:
+    if filename:
         filename = "src/csvs/lorenz.csv"
-    dataset = pd.read_csv(filename)
-    dataset.columns = dataset.columns.str.replace(' ', '')
-    data = dataset[dataset['particle'] == indx]
-    
+        dataset = pd.read_csv(filename)
+        dataset.columns = dataset.columns.str.replace(' ', '')
+        data = dataset[dataset['particle'] == indx]
+    elif dataset is not None:
+        data = dataset[dataset['particle'] == indx]
+    else:
+        raise Exception("No values!")
+
+
     t_test = data['t']
     # x_test = data['x']
     # y_test = data['y']
@@ -36,18 +44,18 @@ def py_visualiser(filename=None, seq_pos=None, indx=8,):
     t_max = t_test.max()  # Maximum time value 
 
     # Scaling factor for points
-    scale_factor = 10
+    scale_factor = 400
 
-    test_points = np.array([])
+    test_points = []
     scaled_xy_t = xy_t * scale_factor + width // 2
     
     if has_pred:
-        xy_pred = seq_pos[:, :1]
-        pred_points = np.array([])
+        xy_pred = seq_pos[:, :2]
+        pred_points = []
         scaled_xy_pred = xy_pred * scale_factor + width // 2
 
     # Animation loop
-    for frame in range(len(data)):
+    for frame in range(len(data)-2):
         # Clear the screen
         screen.fill((0, 0, 0))
 
@@ -66,21 +74,20 @@ def py_visualiser(filename=None, seq_pos=None, indx=8,):
         # Add the current point to the list
         # test_points.append((pos_x, pos_y, color_test))
 
-        test_point = np.array(scaled_xy_t.iloc[frame])
-        print(test_point)
-        test_points = np.append(test_points, test_point)
+        test_point = np.array(scaled_xy_t.iloc[frame+2]-np.array([300,300]))
+        
+        test_points.append(test_point)
 
         if has_pred:
-            pred_point = np.array([scaled_xy_pred[frame], colour_pred]) 
-            pred_points = np.append(pred_points, pred_point)
+            pred_point = np.array(scaled_xy_pred[frame]-np.array([300,300]))
+            pred_points.append(pred_point)
 
             
-        
         # Draw all the points on the screen
-        for point in test_points:
-            # print(point[1])
-            pygame.draw.circle(screen, colour_pred, (point[0],point[1]), 1)
-            # pygame.draw.circle(screen, colour_pred, pred_points[0], 1)
+        for i in range(len(test_points)):
+            pygame.draw.circle(screen, colour_test, test_points[i], 2)
+            if has_pred:
+                pygame.draw.circle(screen, colour_pred, pred_points[i], 2)
 
         # Update the display
         pygame.display.flip()
@@ -92,6 +99,5 @@ def py_visualiser(filename=None, seq_pos=None, indx=8,):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
 
-py_visualiser()
+# py_visualiser()
