9086d8aa8cbf0478f585a0239f06b3deef3c3c9a
KlogeBukser <keranchen@protonmail.com>
2023-05-17 23:39:41 +0200

    Update lorenz_rnn.py

--- src/lorenz_rnn.py
+++ src/lorenz_rnn.py
@@ -2,7 +2,7 @@ import numpy as np
 import utils as ut
 import RNN_copy as rnn
 from tensorflow import keras
-from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras import optimizers
 from tensorflow.keras.losses import MeanSquaredError
 import plot_utils
 
@@ -55,62 +55,31 @@ def load_model(model_name):
     return model_lstm
 
 def lorenz_pred(optimizer, len_seq):
-    # ## Constants
-
-    # len_seq = 2
     raw_data, inputs, targets = ut.prep_data('lorenz', len_seq)
 
-train_test, sequenced_train_test = ut.train_test_split(
-    inputs, targets, train_size, len_seq, spacial_dim
-)
+    train_test, sequenced_train_test = ut.train_test_split(
+        inputs, targets, train_size, len_seq, spacial_dim
+    )
 
-train_inputs, test_inputs, train_targets, test_targets = train_test
-(
-    sequenced_train_inputs,
-    sequenced_test_inputs,
-    sequenced_train_targets,
-    sequenced_test_targets,
-) = sequenced_train_test
+    train_inputs, test_inputs, train_targets, test_targets = train_test
+    (
+        sequenced_train_inputs,
+        sequenced_test_inputs,
+        sequenced_train_targets,
+        sequenced_test_targets,
+    ) = sequenced_train_test
 
-
-# train_inputs.shape, train_targets.shape
-# print(sequences_train_inputs), test_targets.shape
-
-
-# ## The rnn
-#
-
-model_lstm = rnn.lstm(n_hidden=32, n_layers=1, input_shape=(2, spacial_dim))
-model_lstm.build(optimizer='adam', loss='mean_squared_error')
-model_lstm.fit(train_inputs, train_targets, n_epochs)
-model_lstm.summary()
-
-model_vanilla = rnn.simple_rnn(n_hidden=32, n_layers=1, input_shape=(2, spacial_dim))
-model_vanilla.build(optimizer='adam', loss='mean_squared_error')
-model_vanilla.fit(train_inputs, train_targets, n_epochs)
-model_vanilla.summary()
-
-
-# model = load_model('multi_1000_32.h5')
-
-pred_seq_lstm = model_lstm.test(sequenced_test_inputs, test_indx)
-pred_seq_vanilla = model_vanilla.test(sequenced_test_inputs, test_indx)
-# model.save('multi.h5')
-
-
-# ## Visualisation
-
-# separate x,y,z
-
-pred_seq_lstm = pred_seq_lstm.reshape(-1, spacial_dim)
-pred_seq_vanilla = pred_seq_vanilla.reshape(-1, spacial_dim)
+    model_vinilla = rnn.Simple_rnn(n_hidden=32, n_layers=1, input_shape=(len_seq, spacial_dim))
+    model_lstm = rnn.Lstm(n_hidden=32, n_layers=1, input_shape=(len_seq, spacial_dim)) 
+    
+    # predicts
+    ic = sequenced_test_inputs[test_indx]
 
     pred_lstm = rnn_alt(model_lstm, optimizer, train_inputs, train_targets, n_epochs, spacial_dim, ic, False)
     pred_vanilla = rnn_alt(model_vinilla, optimizer, train_inputs, train_targets,n_epochs, spacial_dim, ic)
-
-pred_x_vanilla = pred_seq_vanilla[:, 0]
-pred_y_vanilla = pred_seq_vanilla[:, 1]
-pred_z_vanilla = pred_seq_vanilla[:, 2]
+    
+    return sequenced_test_targets, pred_lstm, pred_vanilla
+    
 
 def eval(sequenced_test_targets, pred_lstm, pred_vanilla):
 
@@ -126,26 +95,24 @@ def eval_n_plot(sequenced_test_targets, pred_lstm, pred_vanilla, len_seq, n_epoc
     test_y = sequenced_test_targets[test_indx][:test_steps, 1]
     test_z = sequenced_test_targets[test_indx][:test_steps, 2]
 
-# Create a 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+    # Create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-# Plot the predicted sequence
-ax.plot(pred_x_lstm, pred_y_lstm, pred_z_lstm, label='LSTM Predicted Sequence')
-ax.plot(pred_x_vanilla, pred_y_vanilla, pred_z_vanilla, label='Vanilla Predicted Sequence')
+    # Plot the predicted sequence
+    ax.plot(pred_lstm[:,0], pred_lstm[:, 1], pred_lstm[:, 2], label='LSTM Predicted Sequence')
+    ax.plot(pred_vanilla[:, 0], pred_vanilla[:, 1], pred_vanilla[:, 2], label='Vanilla Predicted Sequence')
+    ax.plot(test_x, test_y, test_z, label='Test Data', linestyle=":")
 
 
-# Plot the test data
-ax.plot(test_x, test_y, test_z, label='Test Data', linestyle='-.')
+    # Set labels and title
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title(f'Predicted Sequence vs Test Data (look back = {len_seq})')
 
-# Set labels and title
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('Predicted Sequence vs Test Data')
-
-# Add a legend
-ax.legend()
+    # Add a legend
+    ax.legend()
 
     # save plot
     file_info = f"./Analysis/figs/lorenz_rnn-{len_seq}-{n_epochs}-{learning_rate:.2f}.pdf"
@@ -176,7 +143,7 @@ len_seqs = range(2,6)
 for i, n_epochs in enumerate(all_n_epochs):
     for j, len_seq in enumerate(len_seqs):
         # for k, lr in enumerate(np.logspace(10e-4,10e-1,4)):
-        optimizer = Adam(learning_rate=10e-2)
+        optimizer = optimizers.Adam(learning_rate=10e-2)
         sequenced_test_targets, pred_lstm, pred_vanilla = lorenz_pred(optimizer, len_seq)
         errs_lstm[i,j], errs_vanilla[i,j] = eval(sequenced_test_targets, pred_lstm, pred_vanilla)
             
